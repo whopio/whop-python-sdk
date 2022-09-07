260be0474ccb48d5fd5ac4ac5cee41529d05c942
Gavin Kline <77706736+gwkline@users.noreply.github.com>
2022-09-07 09:50:53 -0400

    Added version changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='whop',
-    version='3.0',
+    version='4.1',
     license='MIT',
     author="Whop",
     author_email='support@whop.com',
@@ -11,8 +11,8 @@ setup(
     url='https://github.com/whopio/whop-python-sdk',
     keywords='whop',
     install_requires=[
-          'python_dateutil >= 2.5.3',
-          'setuptools >= 21.0.0',
-          'urllib3 >= 1.25.3',
-      ]
+        'python_dateutil >= 2.5.3',
+        'setuptools >= 21.0.0',
+        'urllib3 >= 1.25.3',
+    ]
 )
